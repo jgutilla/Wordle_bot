a18e589f7031a93a3bfefce3b78c3933d1412b90
John Gutilla <john.gutilla@pnmac.com>
2022-11-16 17:20:57 -0600

    more adds

--- utils.py
+++ utils.py
@@ -470,3 +470,14 @@ def past_answers_frequency():
 
     sorted_score = dict(reversed(sorted(score_dict.items(), key=lambda item: item[1])))
     return sorted_score
+
+
+def look_at_guessed_words():
+    guessed_words = open("guessed_words.txt", "r").read().split('\n')
+
+    words_with_no_dupes = [*set(guessed_words)]
+    frequency_dict = {}
+    for word in words_with_no_dupes:
+        frequency_dict[word] = guessed_words.count(word)
+    sorted_frequency  = dict(reversed(sorted(frequency_dict.items(), key=lambda item: item[1])))
+    print(sorted_frequency)
