ca4d39f85460d37e285c7eacddc225923039d804
John Gutilla <john.gutilla@pnmac.com>
2022-11-16 17:20:37 -0600

    Adding method to utils

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-guessed_words = open("guessed_words.txt", "r").read().split('\n')
 
-words_with_no_dupes = [*set(guessed_words)]
-frequency_dict = {}
-for word in words_with_no_dupes:
-    frequency_dict[word] = guessed_words.count(word)
-sorted_frequency  = dict(reversed(sorted(frequency_dict.items(), key=lambda item: item[1])))
-print(sorted_frequency)
+def look_at_guessed_words():
+    guessed_words = open("guessed_words.txt", "r").read().split('\n')
+
+    words_with_no_dupes = [*set(guessed_words)]
+    frequency_dict = {}
+    for word in words_with_no_dupes:
+        frequency_dict[word] = guessed_words.count(word)
+    sorted_frequency  = dict(reversed(sorted(frequency_dict.items(), key=lambda item: item[1])))
+    print(sorted_frequency)
